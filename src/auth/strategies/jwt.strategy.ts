91c61886d0f5cc01d1b692c3cb28b553384b55da
Ibrahim <ibrahim@MacBook-Pro.fritz.box>
2026-08-12 08:21:12 +0200

    refactor: switch JWT user look up from email to ID

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,24 +7,24 @@ import { UsersService } from '../../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        private readonly usersService: UsersService,
-        configService: ConfigService
-    ) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
-        });
+  constructor(
+    private readonly usersService: UsersService,
+    configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: string; email: string }) {
+    const user = await this.usersService.findById(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid token');
     }
 
-    async validate(payload: { sub: string; email: string }) {
-        const user = await this.usersService.findByEmail(payload.email);
-
-        if (!user) {
-            throw new UnauthorizedException('Invalid token');
-        }
-
-        return user;
-    }
+    return user;
+  }
 }
