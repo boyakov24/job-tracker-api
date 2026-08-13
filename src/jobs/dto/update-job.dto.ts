1ee2b785ae49905f117032aa3464492c710dc278
Ibrahim <ibrahim@MacBook-Pro.fritz.box>
2026-08-13 23:08:33 +0200

    fix: update job DTO

--- src/jobs/dto/update-job.dto.ts
+++ src/jobs/dto/update-job.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from "@nestjs/swagger";
-import { CreateJobDto } from "./create-job.dto";
+import { PartialType } from '@nestjs/swagger';
+import { CreateJobDto } from './create-job.dto';
+import { IsOptional, IsUrl, ValidateIf } from 'class-validator';
 
-export class UpdateJobDto extends PartialType(CreateJobDto){}
+export class UpdateJobDto extends PartialType(CreateJobDto) {
+  @ValidateIf((_, value) => value !== '')
+  @IsOptional()
+  @IsUrl()
+  applicationUrl?: string;
+}
